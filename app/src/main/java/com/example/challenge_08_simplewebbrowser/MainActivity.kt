b32fc19312e37d18402502fd997c55217d19d0c7
seunggyu97 <seunggyu97@gmail.com>
2022-02-09 15:20:21 +0900

    주소입력창 웹주소 이동 기능 추가

--- app/src/main/java/com/example/challenge_08_simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/challenge_08_simplewebbrowser/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.challenge_08_simplewebbrowser
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private val addressBar: EditText by lazy{
+        findViewById(R.id.addressBar)
+    }
+
+    private val webView: WebView by lazy{
+        findViewById(R.id.webView)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
+        bindViews()
     }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initViews(){
+        webView.apply(){
+            webViewClient = WebViewClient()
+            settings.javaScriptEnabled = true
+            loadUrl("http://www.google.com")
+        }
+    }
+
+    private fun bindViews(){
+        addressBar.setOnEditorActionListener { textView, i, keyEvent ->
+            if(i == EditorInfo.IME_ACTION_DONE){
+                webView.loadUrl(textView.text.toString())
+            }
+
+            return@setOnEditorActionListener false
+        }
+    }
+
 }
